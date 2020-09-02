d3c8deb27760bbe7a5d1e1c3dfea65e299f8da60
Sam Erdman <SamWise@OtterGames.lan>
2020-09-02 16:57:12 -0700

    adding updated files

--- scraping.py
+++ scraping.py
@@ -1,4 +1,4 @@
-# ## Scraping Articles
+# Scraping Articles
 
 # Import dependencies
 from splinter import Browser
@@ -72,7 +72,7 @@ def mars_news(browser):
 
     return news_title, news_p
 
-# ## Scraping Images
+# Scraping Images
 
 # Declare and define function
 def featured_image(browser):
@@ -110,7 +110,7 @@ def featured_image(browser):
 
     return img_url
 
-# ## Mars Facts
+# Mars Facts
 def mars_facts():
 
     try: 
@@ -131,6 +131,76 @@ def mars_facts():
 
     # Convert stored df back into HTML format so it can be added to a website
     return df.to_html(classes="table table -striped")
+
+# CHALLENGE  
+# Scrape Mars Hemispheres Images
+def mars_hemisphere_images():
+
+    # Create empty list
+    hemispheres = []
+
+    # Visit URL
+    url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
+    browser.visit(url)
+
+    # Click on each of the 4 hemisphere links and store in variable for next step
+    first_click = soup.findAll("div", {"class": itemLink product-item"})
+    first_click.click()
+
+
+I approached this by using the URL from the "sample" link provided when you click through each hemisphere from the main URL you referenced. Then created a def function for each hemisphere.
+11:59
+For example:
+12:02
+def hemisphere_image1(browser):
+    img_url = 'https://astrogeology.usgs.gov/cache/images/f5e372a36edfa389625da6d0cc25d905_cerberus_enhanced.tif_full.jpg'
+    return img_url
+def hemisphere_image2(browser):
+    img_url = "hemisphere_image2 sample url"
+    return img_url
+def hemisphere_image3(browser):
+    img_url = "hemisphere_image3 sample url"
+    return img_url
+def hemisphere_image4(browser):
+    img_url = "hemisphere_image4 sample url"
+    return img_url
+
+
+
+
+
+
+
+    # Find the more info button by searching for text and click it
+    browser.is_element_present_by_text('more info', wait_time=1)
+    more_info_elem = browser.links.find_by_partial_text('more info')
+    more_info_elem.click()
+
+    # Parse the resulting html with soup
+    html = browser.html
+    img_soup = BeautifulSoup(html, 'html.parser')
+
+    try:
+        # Find the relative image url
+        img_url_rel = img_soup.select_one('figure.lede a img').get("src")
+        # tell BeautifulSoup to look inside the <figure class=”lede” /> tag for an <a /> tag, and then look within 
+        # that <a /> tag for an <img /> tag.
+        # Pull the link of the image by pointing BeautifulSoup to where the image will be, instead of grabbing the URL
+        # directly. This way, when NASA updates its image page, the code will still pull the most recent image.
+
+    except AttributeError:
+        return None
+
+    # Use the base URL to create an absolute URL
+    img_url = f'https://www.jpl.nasa.gov{img_url_rel}'
+
+    return img_url
+
+
+
+
+
+
 
 # Tell Flask to run
 if __name__ == "__main__":
